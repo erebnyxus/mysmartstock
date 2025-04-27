130889dc5583709d93fb2ef7a1e33caf9f0a943c
Abel <nblh.nrzm@gmail.com>
2025-04-27 14:54:19 +0800

    Another change

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,44 +3,44 @@ import type { RouteRecordRaw } from 'vue-router';
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    component: () => import('@/layouts/MainLayout.vue'),
     children: [
       {
         path: '',
         name: 'dashboard',
-        component: () => import('pages/DashboardPage.vue'),
+        component: () => import('@/pages/DashboardPage.vue'),
       },
       {
         path: 'products',
         name: 'products',
-        component: () => import('pages/ProductsPage.vue'),
+        component: () => import('@/pages/ProductsPage.vue'),
       },
       {
         path: 'product/:id',
         name: 'product-details',
-        component: () => import('pages/ProductDetailPage.vue'),
+        component: () => import('@/pages/ProductDetailPage.vue'),
         props: true,
       },
       {
         path: 'product-form/:id?',
         name: 'product-form',
-        component: () => import('pages/ProductFormPage.vue'),
+        component: () => import('@/pages/ProductFormPage.vue'),
         props: true,
       },
       {
         path: 'inventory',
         name: 'inventory',
-        component: () => import('pages/InventoryPage.vue'),
+        component: () => import('@/pages/InventoryPage.vue'),
       },
       {
         path: 'reports',
         name: 'reports',
-        component: () => import('pages/ReportsPage.vue'),
+        component: () => import('@/pages/ReportsPage.vue'),
       },
       {
         path: 'settings',
         name: 'settings',
-        component: () => import('pages/SettingsPage.vue'),
+        component: () => import('@/pages/SettingsPage.vue'),
       },
     ],
   },
@@ -49,7 +49,7 @@ const routes: RouteRecordRaw[] = [
   // but you can also remove it
   {
     path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue'),
+    component: () => import('@/pages/ErrorNotFound.vue'),
   },
 ];
 
